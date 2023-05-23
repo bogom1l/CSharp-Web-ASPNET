55483b92b8a28b1d47de7577fd4e0ffbae33219b
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-05-23 17:46:58 +0300

    Update Program.cs

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -9,7 +9,7 @@ namespace WebApplication1
             // Add services to the container.
             builder.Services.AddRazorPages();
 
-            var app = builder.Build();
+            var app = builder.Build(); //
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
