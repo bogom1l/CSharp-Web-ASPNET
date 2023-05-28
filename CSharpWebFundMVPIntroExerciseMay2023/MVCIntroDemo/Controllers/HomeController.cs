1f90b4d2defc9494cb69ce705b1a2173dd742b81
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-05-28 13:06:05 +0300

    exercise initial push

---
+++ CSharpWebFundMVPIntroExerciseMay2023/MVCIntroDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿namespace MVCIntroDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MVCIntroDemo.Models;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello world az sum gotin";
+            ViewData["Message"] = "Hello world az sum pich";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
